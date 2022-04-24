aad9530d182064fe6dcf10e2adcac55eed5844d9
Disembleergon <tom.pekrul1@gmail.com>
2022-04-24 21:10:50 +0200

    rename GameScript to Component

--- SFML_Template/include/GameScript.hpp
+++ SFML_Template/include/GameScript.hpp
@@ -6,32 +6,32 @@
 /*
 To add functionality to the game, create a new Class for every feature,
 for example for PlayerMovement. This class has to extend from this one and it has to override
-the draw and update methods. For the constructor of your GameScript, add sf::RenderWindow &window as first argument,
+the draw and update methods. For the constructor of your component, add sf::RenderWindow &window as first argument,
 and then call the Constructor of this class, like this:
 
-MyScript(sf::RenderWindow &window, someOtherArguments) : GameScript(window)
+MyScript(sf::RenderWindow &window, someOtherArguments) : Component(window)
 {
 }
 
-In the game class, just create one or more objects of your script and in the mainloop,
+In the game class, just create one or more objects of your component(s) and in the mainloop,
 call the update and draw methods of the scripts.
 
 To access the current sf::RenderWindow, just use m_window inside the update or draw method ;-)
 
 */
 
-// abstract class for game scripts
-class GameScript
+// abstract class for components
+class Component
 {
 public:
-    GameScript(sf::RenderWindow& window)
+    Component(sf::RenderWindow& window)
         : m_window(window) {
         // EMPTY
     };
 
-    GameScript() = delete;
+    Component() = delete;
 
-    virtual ~GameScript() = default;
+    virtual ~Component() = default;
     virtual void draw() = 0;
     virtual void update() = 0;
 
